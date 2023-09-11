fa6969fc1eb8a9d670bc84f9c41e727c2baf33de
60jong <rudwhd515@gmail.com>
2023-09-11 10:08:45 +0900

    init

---
+++ src/test/java/site/_60jong/jpashop/JpaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package site._60jong.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
