4ed2f2b104da50ad622503f61fc7f30857709641
60jong <rudwhd515@gmail.com>
2023-09-12 01:30:50 +0900

    feat : MemberService 멤버 회원가입 기능 구현

---
+++ src/main/java/site/_60jong/jpashop/application/service/MemberService.java
@@ -0,0 +1,48 @@
+package site._60jong.jpashop.application.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site._60jong.jpashop.domain.Address;
+import site._60jong.jpashop.domain.Member;
+import site._60jong.jpashop.persistence.repository.MemberRepository;
+
+import java.util.List;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Long join(MemberDto memberDto) {
+        Member member = memberDto.toEntity();
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> membersByName = memberRepository.findByName(member.getUsername());
+
+        if (!membersByName.isEmpty()) {
+            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // DTO
+    @Getter
+    @AllArgsConstructor
+    public static class MemberDto {
+        private String name;
+        private String city;
+        private String street;
+        private String zipcode;
+
+        private Member toEntity() {
+            return new Member(name, new Address(city, street, zipcode));
+        }
+    }
+}
